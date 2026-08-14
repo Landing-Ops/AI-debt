5911edbc543c1a2c3c4f94bbea1fa7bfb832acc9
minwookang5687 <softman007@naver.com>
2026-08-14 17:42:52 +0900

    gg
    gg

--- js/form-page-nude.js
+++ js/form-page-nude.js
@@ -80,11 +80,11 @@
     { who:'서XX님', region:'서울특별시', debt:'5,000만원', pay:'월 13만원', before:71, after:91, img:'./img/ts_1.png',
       quote:'코로나로 가게 매출이 반토막 나며 카드빚이 5천만원까지 불었어요. 다른 곳에선 71% 정도라고 들었는데, 여기서 정밀 진단을 받아보니 91%까지 나왔습니다. 지금은 매달 13만원 정도만 갚고 있어요.' },
     { who:'임XX님', region:'부산광역시', debt:'1억 8천만원', pay:'월 28만원', before:79, after:95, img:'./img/ts_2.png',
-      quote:'전세 사기를 당하고 대출 이자까지 겹쳐 빚이 1억 8천만원을 넘겼습니다. 처음 알아본 79%에서 접으려 했는데, 정밀 진단에서 제 형편이 반영되니 95%까지 올라갔어요. 지금은 한 달 28만원 정도만 갚습니다.' },
+      quote:'전세 사기를 당하고 대출 이자까지 겹쳐 빚이 1억 8천만원을 넘겼습니다. 처음 알아본 79%로도 만족했었는데, 정밀 진단에서 제 형편이 반영되니 95%까지 올라갔어요. 지금은 한 달 28만원 정도만 갚습니다.' },
     { who:'황XX님', region:'대구광역시', debt:'6,000만원', pay:'월 19만원', before:66, after:87, img:'./img/ts_3.png',
-      quote:'부모님 병원비를 카드 돌려막기로 버티다 빚이 6천만원까지 늘었어요. 66%라던 예상을 반신반의했는데, 정밀 진단 결과 87%가 나와 그제야 마음을 놓았습니다. 매달 19만원씩 갚으며 버티고 있어요.' },
+      quote:'부모님 병원비를 카드 돌려막기로 버티다 빚이 6천만원까지 늘었어요. 제 예상 60%정도는 되지 않을까 싶었는데, 정밀 진단 결과 87%가 나와 그제야 마음을 놓았습니다. 매달 19만원씩 갚으며 버티고 있어요.' },
     { who:'송XX님', region:'경기 수원', debt:'1억 4천만원', pay:'월 31만원', before:74, after:92, img:'./img/ts_4.png',
-      quote:'온라인 쇼핑몰 재고를 떠안으며 빚이 1억 4천만원까지 늘었습니다. 부양가족과 생활 여건까지 정밀하게 반영되니, 처음 74%에서 92%로 껑충 뛰었어요. 지금은 한 달 31만원씩 갚고 있습니다.' },
+      quote:'온라인 쇼핑몰 재고를 떠안으며 빚이 1억 4천만원까지 늘었습니다. 부양가족과 생활 여건까지 정밀하게 반영되니, 처음 74%정도 나왔던게 92%로 껑충 뛰었어요. 지금은 한 달 31만원씩 갚고 있습니다.' },
     { who:'권XX님', region:'인천광역시', debt:'3억 9천만원', pay:'월 49만원', before:80, after:95, img:'./img/ts_5.png',
       quote:'동업자에게 사업 빚을 떠안고 잠적당해 4억 가까이 빚이 남았습니다. 혼자 계산한 80%에서 놓쳤던 항목까지 정밀 진단이 잡아주니 95%가 나왔어요. 지금은 매달 49만원 정도만 갚습니다.' },
     { who:'배XX님', region:'광주광역시', debt:'3억 8천만원', pay:'월 148만원', before:69, after:88, img:'./img/ts_6.png',
@@ -109,7 +109,7 @@
               '<blockquote class="ts__quote">' + r.quote + '</blockquote>' +
             '</div>' +
             '<div class="ts__chartWrap">' +
-              '<div class="ts__chartHead"><span>정밀 진단 후 <strong>탕감률 상승폭</strong></span><b>+' + (r.after - r.before) + '%p</b></div>' +
+              '<div class="ts__chartHead"><span>정밀 진단 후 <strong style="font-size: 18px;">탕감률 상승폭</strong></span><b>+' + (r.after - r.before) + '%p</b></div>' +
               '<div class="ts__miniChart" aria-hidden="true" data-before="' + (r.before/100) + '" data-after="' + (r.after/100) + '">' +
                 '<div class="ts__bar ts__bar--before"><span class="ts__barfoot"><em>정밀 진단 전</em><b>' + r.before + '%</b></span></div>' +
                 '<div class="ts__bar ts__bar--after"><span class="ts__barfoot"><em>정밀 진단 후</em><b>' + r.after + '%</b></span></div>' +
